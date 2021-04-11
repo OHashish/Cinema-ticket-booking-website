ecf8824db52352860e5002f935016e13757c6c23
Jaymeson Heller <YOUR UNIVERSITY EMAIL ADDRESS>
2021-04-11 11:13:40 +0100

    Final Version

--- app/models.py
+++ app/models.py
@@ -11,18 +11,20 @@ class User(UserMixin,db.Model):
     
 class Ticket(db.Model):
     id =db.Column(db.Integer,primary_key=True)
+    valid=db.Column(db.Boolean)
     time=db.Column(db.DateTime)
-    screen_id= db.Column(db.Integer,db.ForeignKey('screen.id'))
     age_type=db.Column(db.String(50))
-    price=db.Column(db.Float)
-    seat=db.relationship('Seat',backref='ticket',uselist=False)
+    quantity=db.Column(db.Integer)
+    price=db.Column(db.String(15))
     user_id=db.Column(db.Integer,db.ForeignKey('user.id'))
+    movie_id= db.Column(db.Integer,db.ForeignKey('movie.id'))
+    seat=db.relationship('Seat',backref='ticket',uselist=False)
+    
+
 
 class Screen(db.Model):
     id =db.Column(db.Integer,primary_key=True)
-    number =db.Column(db.Integer)
-    screen_time=db.Column(db.DateTime)
-    tickets=db.relationship('Ticket',backref='screen')
+    screen_time=db.Column(db.String(15))
     seats=db.relationship("Seat",backref='screen',lazy=True)
     movie_id=db.Column(db.Integer, db.ForeignKey('movie.id'))
     
@@ -30,7 +32,6 @@ class Screen(db.Model):
 class Seat(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     position=db.Column(db.String(80))
-    availability =db.Column(db.Boolean)
     ticket_id= db.Column(db.Integer,db.ForeignKey('ticket.id'))
     screen_id=db.Column(db.Integer,db.ForeignKey('screen.id'))
 
@@ -44,7 +45,8 @@ class Movie(db.Model):
     certificate =db.Column(db.String(20))
     runtime =db.Column(db.Integer)
     movie_poster =db.Column(db.String(100))
-    screen_id=db.relationship("Screen",backref='movie', lazy=True)
+    ticket_id=db.relationship('Ticket',backref='movie', lazy=True, uselist=False)
+    screen_id=db.relationship("Screen",backref='movie', lazy=True, uselist=False)
     
     
 
